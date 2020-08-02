6b289e070cd56173a7c2dfab08f036b616df06c2
JBenitoL <jbenitollorens@gmail.com>
2020-08-02 17:12:41 +0200

    If in cleanJ.., bcs tables change unplug_hourTime

--- InfoTrayectosModule.py
+++ InfoTrayectosModule.py
@@ -6,13 +6,19 @@
 
 import numpy as np
 import pandas as pd
+from pyunpack import Archive
 
 
 def getCleanDataframe(filePath):
-    df = getRawDataFrame(filePath)
+     # Unzip and save in save folder as .rar
+    Archive(filePath).extractall("/".join(filePath.split("/")[0:3]))
+    df = getRawDataFrame(filePath[0:-3]+"json")
     df = cleanJsonformat(df)
-
-    return dateFormatting(df)
+    df = dateFormatting(df)
+    # SaveInPickle in new folder where rest of dataframes are
+    saveInPickle(df, "../DataFrames/Tracks/" +
+                 filePath.split("/")[-1].split(".")[0]+".pkl")
+    return 0
 
 
 def getRawDataFrame(filePath):
@@ -23,7 +29,9 @@ def getRawDataFrame(filePath):
 
 def cleanJsonformat(df):
     df['_id'] = df['_id'].apply(lambda x: x['$oid'])
-    df['unplug_hourTime'] = df['unplug_hourTime'].apply(lambda x: x['$date'])
+    if type(df['unplug_hourTime'][0]) == dict:
+        df['unplug_hourTime'] = df['unplug_hourTime'].apply(
+            lambda x: x['$date'])
 
     return df
 
@@ -31,4 +39,12 @@ def cleanJsonformat(df):
 def dateFormatting(df):
     df['unplug_hourTime'] = pd.to_datetime(
         df['unplug_hourTime'], format='%Y-%m-%dT%H:%M:%S')
+    df["unplug_date"] = df["unplug_hourTime"].dt.date
+    df["unplug_time"] = df["unplug_hourTime"].dt.time
+    df["unplug_dayOfWeek"] = df["unplug_hourTime"].dt.dayofweek
+
     return df
+
+
+def saveInPickle(df, path):
+    df.to_pickle(path)
